9a4f223cc47f26b6ccd54ce97819d9b61b464077
jihyun <my0990@naver.com>
2024-06-26 20:47:40 +0900

    delete history

--- app/kiosk/components/KioskTemplate.js
+++ app/kiosk/components/KioskTemplate.js
@@ -3,41 +3,41 @@ import { useRef, useState, useEffect } from 'react';
 import {createBrowserHistory} from 'history';
 export default function Kiosk() {
     const elementRef = useRef(null);
-    const [isFullscreen, setIsFullscreen] = useState(false);
 
 
-    const history = createBrowserHistory();
-    // 1. history라는 상수에 createBrowerHistory 함수를 할당한다.
 
-    const preventGoBack = () => {
-        // 2. custom hook에서 실행될 함수를 생성하고, 함수명을 preventGoBack으로 설정한다.
-        history.push(null, '', history.location.href);
-        // 2-1. 현재 상태를 세션 히스토리 스택에 추가(push)한다.
-        alert('뒤로 갈 수 없닭! 🐓');
-        // 2-2. 토스트 메세지를 출력한다.
-    };
+    // const history = createBrowserHistory();
+    // // 1. history라는 상수에 createBrowerHistory 함수를 할당한다.
 
-    // 브라우저에 렌더링 시 한 번만 실행하는 코드
-    useEffect(() => {
-        (() => {
-            history.push(null, '', history.location.href);
-            // 3. 렌더링 완료 시 현재 상태를 세션 히스토리 스택에 추가(push)한다.
-            window.addEventListener('popstate', preventGoBack);
-            // 3-1. addEventListener를 이용해 "popstate"라는 이벤트를 감지하게 한다.
-            // 3-2. popstate 이벤트를 감지했을 때 preventGoBack 함수가 실행된다.
-        })();
+    // const preventGoBack = () => {
+    //     // 2. custom hook에서 실행될 함수를 생성하고, 함수명을 preventGoBack으로 설정한다.
+    //     history.push(null, '', history.location.href);
+    //     // 2-1. 현재 상태를 세션 히스토리 스택에 추가(push)한다.
+    //     alert('뒤로 갈 수 없닭! 🐓');
+    //     // 2-2. 토스트 메세지를 출력한다.
+    // };
 
-        return () => {
-            window.removeEventListener('popstate', preventGoBack);
-            // 3-3. 렌더링이 끝난 이후엔 eventListner을 제거한다.
-        };
-    }, []);
+    // // 브라우저에 렌더링 시 한 번만 실행하는 코드
+    // useEffect(() => {
+    //     (() => {
+    //         history.push(null, '', history.location.href);
+    //         // 3. 렌더링 완료 시 현재 상태를 세션 히스토리 스택에 추가(push)한다.
+    //         window.addEventListener('popstate', preventGoBack);
+    //         // 3-1. addEventListener를 이용해 "popstate"라는 이벤트를 감지하게 한다.
+    //         // 3-2. popstate 이벤트를 감지했을 때 preventGoBack 함수가 실행된다.
+    //     })();
 
-    useEffect(() => {
-        history.push(null, '', history.location.href);
-        // 4-1. 현재 상태를 세션 히스토리 스택에 추가(push)한다.
-    }, [history.location]);
-    // 4. history.location (pathname)이 변경될때마다,
+    //     return () => {
+    //         window.removeEventListener('popstate', preventGoBack);
+    //         // 3-3. 렌더링이 끝난 이후엔 eventListner을 제거한다.
+    //     };
+    // }, []);
+
+    // useEffect(() => {
+    //     history.push(null, '', history.location.href);
+    //     // 4-1. 현재 상태를 세션 히스토리 스택에 추가(push)한다.
+    // }, [history.location]);
+    // // 4. history.location (pathname)이 변경될때마다,
 
 
 
