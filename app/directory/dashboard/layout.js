21707d9285749b77665bbb3500543bbefcc5bc14
jihyun <my0990@naver.com>
2025-03-09 23:28:27 +0900

    admin remove

--- app/directory/dashboard/layout.js
+++ app/directory/dashboard/layout.js
@@ -3,6 +3,6 @@ import { getServerSession } from "next-auth"
 export default async function Layout({ student, teacher, adminPage }) {
     const session = await getServerSession(authOptions)
 
-    return <>{session?.userId === 'admin' ? adminPage : session?.role === 'teacher' ? teacher : student}</>
+    return <>{session?.role === 'teacher' ? teacher : student}</>
     
   }
