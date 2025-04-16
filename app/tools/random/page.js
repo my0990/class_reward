8827872d9a7803a67d2f92239f3eaa904f27500e
jihyun <my0990@naver.com>
2025-04-16 21:00:32 +0900

    random num

--- app/tools/random/page.js
+++ app/tools/random/page.js
@@ -2,6 +2,8 @@
 import { fetchData } from "@/hooks/swrHooks";
 import { useState, useEffect } from "react";
 import _ from "lodash";
+import { motion, AnimatePresence } from "framer-motion";
+
 export default function Random() {
     const { data: studentData, isLoading: isStudentLoading, isError: isStudentError } = fetchData('/api/fetchStudentData');
     const { data: classData, isLoading: isClassLoading, isError: isClassError } = fetchData('/api/fetchClassData');
@@ -9,7 +11,8 @@ export default function Random() {
     const [pickedStudentArr, setPickedStudentArr] = useState([]);
     const [isClicked, setIsClicked] = useState(false);
     const [selectedStudentArr, setSelectedStudentArr] = useState([]);
-    const [studnetNum, setStudentNum] = useState(1);
+    const [studentNum, setStudentNum] = useState(1);
+    const [rotation, setRotation] = useState(0);
     // const pickRandomStudent = () => {
     //     if (studentData.length === 0) return;
     //     const randomIndex = Math.floor(Math.random() * studentData.length);
@@ -20,23 +23,36 @@ export default function Random() {
     const pickRandomStudent = () => {
         setIsClicked(true)
         const shuffled = [...originalStudentArr].sort(() => 0.5 - Math.random());
-        const selected = shuffled.slice(0, 2);
-        
+        const selected = shuffled.slice(0, studentNum);
+
         // 원본에서 뽑힌 것 제외한 나머지 만들기
-        const selectedStudentArr = originalStudentArr.filter(item => !selected.includes(item));
-    
+        const remain = originalStudentArr.filter(item => !selected.includes(item));
+
         // 상태 업데이트
-        setSelectedStudentArr(prev => [ ...selected]);
+        setSelectedStudentArr([]);
+        setTimeout(() => {
+            setSelectedStudentArr(prev => [...selected]);
+        }, 50);
+
+
         setPickedStudentArr(prev => [...prev, ...selected]);
-        setOriginalStudentArr(selectedStudentArr);
+        setOriginalStudentArr(remain);
     }
     useEffect(() => {
         if (studentData) {
-            console.log(_.shuffle(studentData));
+
             setOriginalStudentArr(_.shuffle(studentData))
         }
     }, [studentData])
 
+    const onRefresh = () => {
+        setRotation((prev) => prev + 360);
+        setSelectedStudentArr([]);
+        setPickedStudentArr([]);
+        setOriginalStudentArr(studentData);
+        setIsClicked(false);
+        
+    }
     if (isStudentLoading || isClassLoading) return <div>Loading data...</div>;
     if (isStudentError || isClassError) return <div>Error loading data</div>;
     const { startExp, commonDifference } = classData.expTable;
@@ -56,51 +72,97 @@ export default function Random() {
     return (
         <div className=" h-[100vh] flex justify-center items-center flex-col">
             {/* <div>{isClicked && selectedStudent.classNumber}</div> */}
-            <div className="flex justify-between w-full text-[1.5rem] px-[16px]">
-                <div>
-                    나온 번호: {pickedStudentArr.map((a,i)=>a.classNumber).join(', ')}
+            <div className="flex justify-between w-full text-[1.5rem] px-[16px] mt-[32px]">
+                <div className="flex items-center">
+                    나온 번호: {pickedStudentArr.map((a, i) => a.classNumber).sort((a, b) => Number(a) - Number(b)).join(', ')}
                 </div>
-                <div >
-                    <input type="checkbox" className="mr-[8px] cursor-pointer" id="dup" />
-                    <label for="dup" className="cursor-pointer">중복</label>
+                <div className="">
+                    {/* <input type="checkbox" className="mr-[8px] cursor-pointer" id="dup" />
+                    <label htmlFor="dup" className="cursor-pointer">중복</label> */}
+                    <button onClick={onRefresh} style={{ transform: `rotate(${rotation}deg)`, transition: "transform 0.5s ease-in-out" }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-9 transition-all hover:scale-110">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                        </svg>
+
+                    </button>
                 </div>
             </div>
-            <div className="min-h-[600px] w-full flex justify-center items-center bg-orange-300 mb-[32px]  flex-wrap">
-                {isClicked &&
+            <div className="h-[600px] w-full flex justify-center items-center bg-orange-300 mb-[32px] py-[48px] flex-wrap overflow-y-scroll">
+                {isClicked ?
                     selectedStudentArr.map((selectedStudent, i) => {
-                        console.log(selectedStudent)
                         return (
-                            <div >
-                                <div className="shadow-[4px_6px_0px_0px_#050071] w-[160px] p-[16px] m-[8px] bg-orange-200 rounded-xl cursor-pointer ">
-                                    <div className="flex justify-between mb-[8px]">
-                                        <div className="font-semibold">LV. {findLargestSumUnderTarget(selectedStudent)}</div>
-                                        <div className="w-[80px] text-right overflow-hidden whitespace-nowrap"> </div>
-                                    </div>
-                                    <div className="flex overflow-hidden border-4 border-white justify-center items-center w-[110px] h-[110px] mb-[8px] mx-auto bg-white rounded-full">
-                                        <img src={selectedStudent.profileUrl} width="100" height="100" alt="characther" className="rounded-full" />
-                                    </div>
-                                    <h1 className="text-[1rem] font-bold text-center overflow-hidden">{selectedStudent.classNumber}. {selectedStudent.profileNickname}</h1>
-                                    <div className="bg-white rounded-lg flex items-center justify-center">
-                                        {selectedStudent.profileTitle}
+                            <motion.div
+                                key={i}
+                                initial={{ opacity: 0, scale: 0.8 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0, scale: 0.5 }}
+                                transition={{ delay: i * 0.3, duration: 0.5, ease: "easeOut" }}
+                            >
+                                <div key={i}>
+                                    <div className="shadow-[4px_6px_0px_0px_#050071] w-[160px] p-[16px] m-[8px] bg-orange-200 rounded-xl cursor-pointer ">
+                                        <div className="flex justify-between mb-[8px]">
+                                            <div className="font-semibold">LV. {findLargestSumUnderTarget(selectedStudent)}</div>
+                                            <div className="w-[80px] text-right overflow-hidden whitespace-nowrap"> </div>
+                                        </div>
+                                        <div className="flex overflow-hidden border-4 border-white justify-center items-center w-[110px] h-[110px] mb-[8px] mx-auto bg-white rounded-full">
+                                            <img src={selectedStudent.profileUrl} width="100" height="100" alt="characther" className="rounded-full" />
+                                        </div>
+                                        <h1 className="text-[1rem] font-bold text-center overflow-hidden">{selectedStudent.classNumber}. {selectedStudent.profileNickname}</h1>
+                                        <div className="bg-white rounded-lg flex items-center justify-center">
+                                            {selectedStudent.profileTitle}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </motion.div>
                         )
                     })
-                }
+                :<div className="text-[2rem]">뽑기 버튼을 클릭해주세요</div>}
             </div>
-            <div className="flex items-center">
-                <div onClick={() => setStudentNum(prev => prev - 1)} className="cursor-pointer transition-all hover:scale-110 w-[30px] h-[30px] p-[4px] border-2 border-black rounded-full flex justify-center items-center bg-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" /></svg>
+            <div className="flex items-center mb-[32px]">
+                <div onClick={() => setStudentNum(prev => prev > 1 ? prev - 1 : 1)} className="cursor-pointer transition-all hover:scale-110">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-9">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+
                 </div>
-                <button onClick={pickRandomStudent} className="mx-[16px] inline-flex items-center gap-2 transition duration-300 ease-linear 
+                {/* <button onClick={pickRandomStudent} className="mx-[16px] inline-flex items-center gap-2 transition duration-300 ease-linear 
                 bg-[#5751E1] text-white text-lg font-semibold font-['Poppins',sans-serif] leading-[1.12] 
                 px-8 py-4 text-center capitalize select-none rounded-full
-                whitespace-nowrap hover:bg-red-500 hover:shadow-none">{studnetNum}명 뽑기</button>
-                <div onClick={() => setStudentNum(prev => prev + 1)} className="cursor-pointer transition-all hover:scale-110 w-[30px] h-[30px] p-[4px] border-2 border-black rounded-full flex justify-center items-center bg-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" /></svg>
+                whitespace-nowrap hover:bg-red-500 hover:shadow-none">
+                    {studentNum}명 뽑기</button> */}
+
+                <motion.button
+                    whileHover={{
+                        backgroundColor: [
+                            "#fcd34d",
+                            "#fb7185",
+                            "#a5f3fc",
+                            "#c4b5fd",
+                            "#bbf7d0",
+                            "#fcd34d",
+                        ],
+                        color: "#000", // hover 시 텍스트를 검정으로 바꿔서 대비 좋게
+                        scale: 1,
+                        boxShadow: "0 0 20px rgba(255,255,255,0.7)",
+                        transition: {
+                            duration: 2,
+                            repeat: Infinity,
+                        },
+                    }}
+                    className="mx-[16px] select-none relative z-20 px-6 py-3 bg-yellow-300 text-black font-bold text-lg rounded-full shadow-md overflow-hidden"
+                    onClick={pickRandomStudent}
+                >
+                    {studentNum}명 뽑기
+                </motion.button>
+                <div onClick={() => setStudentNum(prev => prev + 1)} className="cursor-pointer transition-all hover:scale-110">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-9">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+
+
                 </div>
             </div>
+
         </div>
     )
 }
